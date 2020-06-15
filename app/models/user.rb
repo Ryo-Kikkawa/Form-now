eb9a208ac81f85e32ca5d87b54c5f7681038a857
Ryo-Kikkawa <l.lawliet.822k@gmail.com>
2020-06-15 15:51:50 +0900

    strat

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,4 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-         validates :name, presence: true, uniqueness: true
-         validates :income, presence: true
-         validates :spending, presence: true
-         validates :target, presence: true
-         validates :target_amount, presence: true
 end
--- db/migrate/20200610055643_devise_create_users.rb
+++ db/migrate/20200610055643_devise_create_users.rb
@@ -4,12 +4,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false
-      t.integer :income,            null: false  
-      t.integer :pending,           null: false
-      t.string :target,             null: false
-      t.string :target_amount,      null: false            
+      t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
@@ -38,7 +35,8 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.0]
 
       t.timestamps null: false
     end
-    add_index :users, :name, unique: true
+
+    add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
